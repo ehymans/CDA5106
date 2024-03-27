5865d6ebff40ae7a1070eefd905d05ad0df3f3a2
KShervington <kyle.shervington@gmail.com>
2024-03-27 18:34:07 -0400

    L2 stats print conditional + placeholder fifo code

--- sim_cache.cpp
+++ sim_cache.cpp
@@ -13,15 +13,12 @@ TO-DO LIST:
   1) For debug5.txt, L2 output statistics are incorrect. The # of reads/writes is lower than what is expected. The expected results are at the end of debug5.txt.
   2) FIFO & Optimal Replacement Polciies
         - FIFO might involve using a queue of block index's
-  3) Handling INCLUSIVE Cache. 
+  3) Handling INCLUSIVE Cache.
   4) Moving from L1 to L2 cache has some problems like evicting the block and passing it to L2
-  5) Overall structure, checking the parameters before different actions are done on the cache (LRU, FIFO, evicting, replacing, etc) 
+  5) Overall structure, checking the parameters before different actions are done on the cache (LRU, FIFO, evicting, replacing, etc)
 
   ...
 */
-
-
-
 
 #include <iostream>
 #include <vector>
@@ -34,7 +31,7 @@ TO-DO LIST:
 
 using namespace std;
 
-class CacheLine 
+class CacheLine
 {
 public:
     long long tag;
@@ -42,21 +39,22 @@ public:
     CacheLine() : tag(-1), dirty(false) {}
 };
 
-class CacheSet 
+class CacheSet
 {
 public:
     vector<CacheLine> lines;
     vector<long long> lru_position;
-    vector<int> fifo_position;          // this may need to be changed from an int
+    vector<int> fifo_position; // this may need to be changed from an int
 
-    CacheSet(int assoc) : lines(assoc), lru_position(assoc), fifo_position(assoc, 0) 
+    CacheSet(int assoc) : lines(assoc), lru_position(assoc), fifo_position(assoc, 0)
     {
-        for(int i = 0; i < assoc; ++i) {
+        for (int i = 0; i < assoc; ++i)
+        {
             lru_position[i] = i;
         }
     }
 
-    void replace_block_lru(long long tag) 
+    void replace_block_lru(long long tag)
     {
         // LRU implementation: Find least recently used block and replace it with new tag
         int lru_index = distance(lru_position.begin(), min_element(lru_position.begin(), lru_position.end()));
@@ -64,23 +62,23 @@ public:
         // need to include dirty bit handling
     }
 
-    void replace_block_fifo(long long tag) 
+    void replace_block_fifo(long long tag)
     {
         // FIFO implementation: Replace the oldest block
         int fifo_index = fifo_position.front();
-        fifo_position.erase(fifo_position.begin());     // Remove the oldest block's index
-        fifo_position.push_back(fifo_index);            // Add it back to signify it's the newest
+        fifo_position.erase(fifo_position.begin()); // Remove the oldest block's index
+        fifo_position.push_back(fifo_index);        // Add it back to signify it's the newest
         lines[fifo_index].tag = tag;
         // need to include dirty bit handling
     }
 
-    void replace_block_optimal(long long tag, const vector<long long>& future_references) 
+    void replace_block_optimal(long long tag, const vector<long long> &future_references)
     {
-        // gotta figure this one out 
+        // gotta figure this one out
     }
 };
 
-class Cache 
+class Cache
 {
 private:
     vector<CacheSet> sets;
@@ -96,9 +94,9 @@ private:
     unsigned long long writes_count = 0;
 
 public:
-    Cache(unsigned int size, unsigned int assoc, unsigned int block_size, unsigned int replacement, unsigned int inclusion) : 
-        assoc(assoc), block_size(block_size), replacement_policy(replacement), inclusion_policy(inclusion) {
-        num_sets = size / (block_size * assoc); // Use block_size here
+    Cache(unsigned int size, unsigned int assoc, unsigned int block_size, unsigned int replacement, unsigned int inclusion) : assoc(assoc), block_size(block_size), replacement_policy(replacement), inclusion_policy(inclusion)
+    {
+        num_sets = size == 0 ? 0 : size / (block_size * assoc); // Use block_size here
         sets.resize(num_sets, CacheSet(assoc));
     }
 
@@ -114,11 +112,11 @@ public:
 
     ////////////////////////////////////////////////
     ////////////// EVICT_BLOCK /////////////////////
-    ////////////////////////////////////////////////   
-    void evict_block(int set_index, int block_index) 
+    ////////////////////////////////////////////////
+    void evict_block(int set_index, int block_index)
     {
         // Assuming non-inclusive cache, thus not checking L1
-        if (sets[set_index].lines[block_index].dirty) 
+        if (sets[set_index].lines[block_index].dirty)
         {
             // increment the write-back counter
             writebacks++;
@@ -131,7 +129,7 @@ public:
     ////////////////////////////////////////////////
     ////////////// UPDATE_LRU //////////////////////
     ////////////////////////////////////////////////
-    void update_lru(int set_index, int index) 
+    void update_lru(int set_index, int index)
     {
         // Move the accessed block to the most recently used position
         long long lru_tag = sets[set_index].lru_position[index];
@@ -140,19 +138,32 @@ public:
     }
 
     ////////////////////////////////////////////////
+    ////////////// UPDATE_FIFO //////////////////////
+    ////////////////////////////////////////////////
+    void update_fifo(int set_index, int index)
+    {
+        long long fifo_tag = sets[set_index].fifo_position[index];
+        int fifo_index = sets[set_index].fifo_position.front();
+
+        sets[set_index].fifo_position.erase(sets[set_index].fifo_position.begin()); // Remove the oldest block's index
+        sets[set_index].fifo_position.push_back(fifo_index);                        // Add it back to signify it's the newest
+        sets[set_index].lines[fifo_index].tag = fifo_tag;
+    }
+
+    ////////////////////////////////////////////////
     ////////////// ALLOCATE_BLOCK //////////////////
     ////////////////////////////////////////////////
-    void allocate_block(int set_index, long long tag, char op) 
+    void allocate_block(int set_index, long long tag, char op)
     {
         bool foundEmptyLine = false;
-        for (int i = 0; i < assoc; ++i) 
+        for (int i = 0; i < assoc; ++i)
         {
-            if (sets[set_index].lines[i].tag == -1) 
+            if (sets[set_index].lines[i].tag == -1)
             { // Empty line found
                 sets[set_index].lines[i].tag = tag;
-                if (op == 'w') 
+                if (op == 'w')
                 {
-                    sets[set_index].lines[i].dirty = true;  // Mark as dirty if it's a write operation --> this is important for WBWA
+                    sets[set_index].lines[i].dirty = true; // Mark as dirty if it's a write operation --> this is important for WBWA
                 }
                 update_lru(set_index, i); // Update LRU since it's a new entry
                 foundEmptyLine = true;
@@ -162,13 +173,13 @@ public:
 
         if (!foundEmptyLine)
         {
-            if(replacement_policy == 0)
+            if (replacement_policy == 0)
             {
-                
+
                 // Evict a block if no empty line is found
                 int lru_index = distance(sets[set_index].lru_position.begin(), max_element(sets[set_index].lru_position.begin(), sets[set_index].lru_position.end()));
-                
-                if (sets[set_index].lines[lru_index].dirty) 
+
+                if (sets[set_index].lines[lru_index].dirty)
                 {
                     writebacks++;
                 }
@@ -177,15 +188,24 @@ public:
                 sets[set_index].lines[lru_index].dirty = (op == 'w'); // Set dirty based on operation
                 update_lru(set_index, lru_index);
             }
-            else if(replacement_policy == 1)
+            else if (replacement_policy == 1)
             {
                 // FIFO
+                int fifo_index = sets[set_index].fifo_position.front();
+
+                if (sets[set_index].lines[fifo_index].dirty)
+                {
+                    writebacks++;
+                }
+
+                sets[set_index].lines[fifo_index].tag = tag;
+                sets[set_index].lines[fifo_index].dirty = (op == 'w');
+                update_fifo(set_index, fifo_index);
             }
-            else if(replacement_policy == 2)
+            else if (replacement_policy == 2)
             {
                 // OPTIMAL
             }
-
         }
     }
 
@@ -193,31 +213,34 @@ public:
     ////////////// SIMULATE_ACCESS /////////////////
     ////////////////////////////////////////////////
 
-    bool simulate_access(char op, long long address) 
+    bool simulate_access(char op, long long address)
     {
         int log_block_size = static_cast<int>(log2(block_size));
         int set_index = (address >> log_block_size) % num_sets;
         long long tag = address >> (log_block_size + static_cast<int>(log2(num_sets)));
 
         // Increment reads or writes count based on operation type
-        if (op == 'r') 
+        if (op == 'r')
         {
             reads_count++;
-            //cout << "Read counter Incremented: " << reads_count << "\n";
-        } 
-        else if (op == 'w') 
+            // cout << "Read counter Incremented: " << reads_count << "\n";
+        }
+        else if (op == 'w')
         {
             writes_count++;
         }
 
         // Search for the tag in the set
         bool hit = false;
-        for (int i = 0; i < assoc; i++) {
-            if (sets[set_index].lines[i].tag == tag) {
+        for (int i = 0; i < assoc; i++)
+        {
+            if (sets[set_index].lines[i].tag == tag)
+            {
                 // Hit found
                 hit = true;
                 hit_count++;
-                if (op == 'w') {
+                if (op == 'w')
+                {
                     sets[set_index].lines[i].dirty = true;
                 }
                 update_lru(set_index, i);
@@ -225,15 +248,16 @@ public:
             }
         }
 
-        if (!hit) {
+        if (!hit)
+        {
             // Miss
             miss_count++;
             allocate_block(set_index, tag, op);
-            if (op == 'r') 
+            if (op == 'r')
             {
                 read_misses++;
-            } 
-            else if (op == 'w') 
+            }
+            else if (op == 'w')
             {
                 write_misses++;
             }
@@ -242,114 +266,127 @@ public:
         return hit;
     }
 
-
-    void print_statistics() 
+    void print_statistics()
     {
         // Updated to print additional required statistics
         unsigned long long accesses = reads_count + writes_count;
         float miss_rate = accesses > 0 ? static_cast<float>(read_misses + write_misses) / accesses : 0;
-        
+
         cout << "Read operations: " << reads_count << "\n";
         cout << "Read Misses: " << read_misses << "\n";
         cout << "Write operations: " << writes_count << "\n";
         cout << "Write Misses: " << write_misses << "\n";
         cout << "Writebacks: " << writebacks << "\n";
         cout << "Miss Rate: " << fixed << setprecision(2) << (accesses > 0 ? miss_rate * 100 : 0) << "%\n";
-        //cout << "Miss rate: " << static_cast<float>(miss_count) / (hit_count + miss_count) * 100 << "%\n";
+        // cout << "Miss rate: " << static_cast<float>(miss_count) / (hit_count + miss_count) * 100 << "%\n";
     }
 
-
     // New method to output the final contents of the cache - 3/25/24
-    void print_contents() 
+    void print_contents()
     {
         cout << "Final Cache Contents:\n";
-        for (unsigned long long i = 0; i < num_sets; ++i) {
+        for (unsigned long long i = 0; i < num_sets; ++i)
+        {
             cout << "Set " << i << ":";
-            for (auto& line : sets[i].lines) {
+            for (auto &line : sets[i].lines)
+            {
                 cout << " [" << (line.tag == -1 ? "Empty" : to_string(line.tag)) << (line.dirty ? " D" : "") << "]";
             }
             cout << "\n";
         }
     }
-
 };
 
-class Simulation 
+class Simulation
 {
 private:
     Cache L1_cache;
-    Cache L2_cache; 
+    Cache L2_cache;
     string trace_file;
+
 public:
     // constructor to initialize both L1 and L2 caches
-    Simulation(unsigned int block_size, unsigned int L1_size, unsigned int L1_assoc, unsigned int L2_size, unsigned int L2_assoc, unsigned int replacement, unsigned int inclusion, const string& trace_file) :
-        L1_cache(L1_size, L1_assoc, block_size, replacement, inclusion), 
-        L2_cache(L2_size, L2_assoc, block_size, replacement, inclusion), // Pass block_size here
-        trace_file(trace_file) {}
-
-
-    void run() 
+    Simulation(unsigned int block_size, unsigned int L1_size, unsigned int L1_assoc, unsigned int L2_size, unsigned int L2_assoc, unsigned int replacement, unsigned int inclusion, const string &trace_file) : L1_cache(L1_size, L1_assoc, block_size, replacement, inclusion),
+                                                                                                                                                                                                                L2_cache(L2_size, L2_assoc, block_size, replacement, inclusion), // Pass block_size here
+                                                                                                                                                                                                                trace_file(trace_file)
     {
+    }
+
+    void run()
+    {
+        bool l2exists = L2_cache.getNumSets() != 0;
+
         cout << "Memory Hierarchy Configuration and Trace Filename:\n";
-        cout << "L1 Cache: " << L1_cache.getNumSets() * L1_cache.getAssoc() * L1_cache.getBlockSize() / 1024 << "KB " 
-            << L1_cache.getAssoc() << "-way, Block size: " << L1_cache.getBlockSize() << "B\n";
-        cout << "L2 Cache: " << L2_cache.getNumSets() * L2_cache.getAssoc() * L2_cache.getBlockSize() / 1024 << "KB " 
-            << L2_cache.getAssoc() << "-way, Block size: " << L2_cache.getBlockSize() << "B\n";
+        cout << "L1 Cache: " << L1_cache.getNumSets() * L1_cache.getAssoc() * L1_cache.getBlockSize() / 1024 << "KB "
+             << L1_cache.getAssoc() << "-way, Block size: " << L1_cache.getBlockSize() << "B\n";
+
+        if (l2exists)
+        {
+            cout << "L2 Cache: " << L2_cache.getNumSets() * L2_cache.getAssoc() * L2_cache.getBlockSize() / 1024 << "KB "
+                 << L2_cache.getAssoc() << "-way, Block size: " << L2_cache.getBlockSize() << "B\n";
+        }
+
         cout << "Trace file: " << trace_file << "\n";
 
         ifstream inp(trace_file);
-        if (!inp) {
+        if (!inp)
+        {
             cerr << "Error opening trace file\n";
             return;
         }
 
         char op;
         long long address;
-        while (inp >> op >> hex >> address) 
+        while (inp >> op >> hex >> address)
         {
-            //cout << "Operation: " << op << ", Address: " << address << "\n"; // Debug print
-            // First, attempt access in L1 cache
-            if (!L1_cache.simulate_access(op, address)) 
+            // cout << "Operation: " << op << ", Address: " << address << "\n"; // Debug print
+            //  First, attempt access in L1 cache
+            if (!L1_cache.simulate_access(op, address) && l2exists)
             {
-                // If miss in L1, access L2 cache
+                // If miss in L1, access L2 cache when it exists
                 L2_cache.simulate_access(op, address);
             }
         }
 
         inp.close();
 
-        /*
         // Print the final contents of all caches
-        cout << "L1 Cache Contents:\n";
-        L1_cache.print_contents();
-        cout << "L2 Cache Contents:\n";
-        L2_cache.print_contents();*/
+        // cout << "L1 Cache Contents:\n";
+        // L1_cache.print_contents();
+        // cout << "L2 Cache Contents:\n";
+        // L2_cache.print_contents();
 
         // Print additional stats
         cout << "\nL1 Cache Statistics\n";
-        L1_cache.print_statistics();    // L1 stats
-        cout << "\nL2 Cache Statistics\n";
-        L2_cache.print_statistics();    // L2 stats
+        L1_cache.print_statistics(); // L1 stats
+
+        if (l2exists)
+        {
+            cout << "\nL2 Cache Statistics\n";
+            L2_cache.print_statistics(); // L2 stats
+        }
     }
 };
-
 
 /*////////////////////////////////////////////////
 ********************* MAIN **********************
 ///////////////////////////////////////////////*/
-int main(int argc, char* argv[]) 
+int main(int argc, char *argv[])
 {
     /* 8KB 4-way set-associative L1 cache with 32B block size, 256KB 8-way set-associative L2
-    * cache with 32B block size, LRU replacement, non-inclusive cache (default), gcc trace:
-    * 
-    * ./sim_cache 32 8192 4 262144 8 0 0 traces/gcc_trace.txt
-    * 
-    * debug5.txt comparision testing: ./sim_cache 16 1024 1 8192 4 0 0 traces/go_trace.txt
-    */
+     * cache with 32B block size, LRU replacement, non-inclusive cache (default), gcc trace:
+     *
+     * ./sim_cache 32 8192 4 262144 8 0 0 traces/gcc_trace.txt
+     *
+     * debug5.txt comparision testing: ./sim_cache 16 1024 1 8192 4 0 0 traces/go_trace.txt
+     */
 
-    if (argc != 9) 
+    //  FIFO testing
+    // debug2.txt: ./sim_cache 16 1024 2 0 0 1 0 traces/gcc_trace.txt
+
+    if (argc != 9)
     {
-        cerr << "Usage: <BLOCKSIZE> <L1_SIZE> <L1_ASSOC> <L2_SIZE> <L2_ASSOC> <REPLACEMENT_POLICY> <INCLUSION_POLICY> <TRACE_FILE>\n";
+        cerr << "Usage: <BLOCKSIZE> <L1_SIZE> <L1_ASSOC> <L2_SIZE> <L2_ASSOC> <REPLACEMENT_POLICY> <INCLUSION_POLICY> <TRACE_FILE>\nInput: " << argv[1];
 
         return 1;
     }
@@ -364,7 +401,6 @@ int main(int argc, char* argv[])
     unsigned int inclusion_policy = stoi(argv[7]);
     string trace_file = argv[8];
 
-    
     Simulation sim(block_size, L1_size, L1_assoc, L2_size, L2_assoc, replacement_policy, inclusion_policy, trace_file);
     sim.run();
 
